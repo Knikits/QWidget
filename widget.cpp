0fc5b5915202f2f17d6f645846fd9b0b4f148598
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-04-08 14:03:08 +0300

    All comments for 2.8

--- widget.cpp
+++ widget.cpp
@@ -1,33 +1,33 @@
 #include "widget.h"
 #include "ui_widget.h"
 
-Widget::Widget(QWidget *parent)
+Widget::Widget(QWidget *parent) // конструктор
     : QWidget(parent)
 {
-     this->setWindowTitle("Счетчик");
-     label1 = new QLabel("Cчет по 1",this);
-     label2 = new QLabel("Cчет по 5",this);
-     edit1 = new Counter("0",this);
-     edit2 = new Counter("0",this);
-     calcbutton=new QPushButton("+1",this);
-     exitbutton=new QPushButton("Выход",this);
-     QHBoxLayout *layout1 = new QHBoxLayout();
-     layout1->addWidget(label1);
-     layout1->addWidget(label2);
-     QHBoxLayout *layout2 = new QHBoxLayout();
-     layout2->addWidget(edit1);
-     layout2->addWidget(edit2);
-     QHBoxLayout *layout3 = new QHBoxLayout();
-     layout3->addWidget(calcbutton);
-     layout3->addWidget(exitbutton);
-     QVBoxLayout *layout4 = new QVBoxLayout(this);
+     this->setWindowTitle("Счетчик"); // Название окна
+     label1 = new QLabel("Cчет по 1",this); // название текстового поля
+     label2 = new QLabel("Cчет по 5",this); // название текстового поля
+     edit1 = new Counter("0",this); // выставляем в поле вывода 0
+     edit2 = new Counter("0",this); // выставляем в поле вывода 0
+     calcbutton=new QPushButton("+1",this); // добавляем кнопку +1
+     exitbutton=new QPushButton("Выход",this); // добавляем кнопку Выход
+     QHBoxLayout *layout1 = new QHBoxLayout(); // создаем горизонтальный макет
+     layout1->addWidget(label1); // заполняем его текстовыми полями (название)
+     layout1->addWidget(label2); // заполняем его текстовыми полями (название)
+     QHBoxLayout *layout2 = new QHBoxLayout(); // создаем второй горизонтальный макет
+     layout2->addWidget(edit1); // поле вывода
+     layout2->addWidget(edit2);// поле вывода
+     QHBoxLayout *layout3 = new QHBoxLayout(); // создаем третий горизонтальный макет
+     layout3->addWidget(calcbutton); // кнопка +1
+     layout3->addWidget(exitbutton); // кнопка Выход
+     QVBoxLayout *layout4 = new QVBoxLayout(this); // // создаем вертикальный макет, в который добаляем все горизонтальные макеты
      layout4->addLayout(layout1);
      layout4->addLayout(layout2);
      layout4->addLayout(layout3);
      // связь сигнала нажатия кнопки и слота закрытия окна
-     connect(calcbutton,SIGNAL(clicked(bool)), edit1,SLOT(add_one()));
-     connect(edit1,SIGNAL(tick_signal()), edit2,SLOT(add_one()));
-     connect(exitbutton,SIGNAL(clicked(bool)), this,SLOT(close()));
+     connect(calcbutton,SIGNAL(clicked(bool)), edit1,SLOT(add_one())); // сигнал нажатия кнопки +1 вызывает функцию добавления единицы
+     connect(edit1,SIGNAL(tick_signal()), edit2,SLOT(add_one())); // связываем тик одного сигнала из класса counter и добавление единицы
+     connect(exitbutton,SIGNAL(clicked(bool)), this,SLOT(close())); // сигнал нажатия кнопки Выход вызывает выход из окна
 }
 
 
--- widget.h
+++ widget.h
@@ -3,45 +3,43 @@
 
 #include <QWidget>
 #include <QtGui>
-#include <QFrame>
-#include <QLabel>
+#include <QLabel>     // компоненты
 #include <QLineEdit>
 #include <QPushButton>
-#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Counter; }
 QT_END_NAMESPACE
 
-class Counter : public QLineEdit
+class Counter : public QLineEdit // класс счетчика является наследником класса QLineEdit
 {
-    Q_OBJECT
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
 public:
-    Counter(const QString & contents, QWidget *parent=0): QLineEdit(contents,parent){}
+    Counter(const QString & contents, QWidget *parent=0): QLineEdit(contents,parent){} // конструтор
 signals:
-    void tick_signal();
+    void tick_signal(); // функция тика типа void (сигнал для вызова)
 public slots:
-    void add_one()
+    void add_one() // функция увеличения на единицу
     {
-        QString str = text();
-        int r=str.toInt();
-        if (r!=0 && r%5 ==0) emit tick_signal();
-        r++;
-        str.setNum(r);
-        setText(str);
+        QString str = text(); // берём всё, что есть в поле ввода/вывода
+        int r = str.toInt(); // преобразовываем в int
+        if (r!=0 && r%5 ==0) emit tick_signal(); // проверка условия, если не равно нулю и делится нацело на пять, то вызываем сигнал
+        r++; // увеличиваем на единицу
+        str.setNum(r); // преобразовываем r в QString
+        setText(str); // устанавливам r в текстовое поле
      }
 };
 
-class Widget: public QWidget
+class Widget: public QWidget // класс окна является наследником класса QWidget
 {
-    Q_OBJECT
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
 protected:
-    QLabel *label1, *label2;
-    Counter *edit1, *edit2;
-    QPushButton *calcbutton;
-    QPushButton *exitbutton;
+    QLabel *label1, *label2; // два текстовоых (зоголовчных) поля
+    Counter *edit1, *edit2; // поля вывода
+    QPushButton *calcbutton; // кнопка +1
+    QPushButton *exitbutton; // кнопка выхода
 public:
-    Widget(QWidget *parent = 0);
+    Widget(QWidget *parent = 0); // конструтор
 };
 
 #endif // WIDGET_H
